8f6ed6285ef7fb77e58b235d2c8995d4ad3c8582
HoangVinhJixJi <nguyenhoangvv284@gmail.com>
2024-03-30 15:46:28 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Static file
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')))
 
 // Template engine
 
-app.engine(
+  app.engine(
   'hbs',
   handlebars.engine({
     extname: '.hbs',
